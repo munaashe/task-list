489b0cce4eb2e9f00e675991652fb6c202fe67f1
Denis Siduna <125746473+munaashe@users.noreply.github.com>
2024-12-12 16:07:52 +0200

    style the taskCard

--- components/task-card/index.tsx
+++ components/task-card/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Card, Button } from 'react-native-paper';
+import { Card, RadioButton } from 'react-native-paper';
 import Text from '../text';
 
 interface TaskCardProps {
@@ -13,13 +13,27 @@ const TaskCard: React.FC<TaskCardProps> = ({ title, completed, onPress }) => {
     return (
         <Card style={[styles.card, completed && styles.completedCard]}>
             <Card.Content>
-                <Text size="medium" weight="normal" color="black">{title}</Text>
+                <View style={styles.cardContent}>
+                    <View style={styles.title}>
+                        <Text size="medium" weight="normal" color="black">
+                            {title}
+                        </Text>
+                    </View>
+                    <View style={styles.completedContainer}>
+                        <Text size="small" weight="normal" color="black" >
+                            Complete?
+                        </Text>
+                        <RadioButton
+                            value="checked"
+                            status={completed ? 'checked' : 'unchecked'}
+                            onPress={onPress}
+                            color='black'
+                            theme={{ roundness: 50 }}
+
+                        />
+                    </View>
+                </View>
             </Card.Content>
-            <Card.Actions>
-                <Button mode="contained" onPress={onPress}>
-                    {completed ? 'Completed' : 'Mark as Complete'}
-                </Button>
-            </Card.Actions>
         </Card>
     );
 };
@@ -33,6 +47,24 @@ const styles = StyleSheet.create({
     },
     completedCard: {
         backgroundColor: '#d4edda',
+    },
+    cardContent: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        gap: 8,
+    },
+    title: {
+        width: "80%",
+    },
+    completedContainer: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 2,
+    },
+    completeText: {
+        marginRight: 8,
     },
 });
 
